ebbd776eb8ff6f308d405def506966f13f2e1780
teovoinea <voinea.teodor@gmail.com>
2018-03-05 20:55:00 -0500

    Can draw in colour now

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::rc::Rc;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::u8::MAX;
+use std::i64;
 
 use bincode::{serialize, deserialize};
 
@@ -22,9 +23,11 @@ use stdweb::web::{
     ArrayBuffer,
     TypedArray,
     SocketBinaryType,
+    CanvasStyle,
 };
 
 use stdweb::web::event::{
+    ChangeEvent,
     MouseMoveEvent,
     MouseDownEvent,
     MouseUpEvent,
@@ -74,10 +77,26 @@ fn main() {
     ws.set_binary_type(SocketBinaryType::ArrayBuffer);
 
     let canvas: CanvasElement = document().query_selector( "#canvas" ).unwrap().unwrap().try_into().unwrap();
+    let paint_color: HtmlElement = document().query_selector( "#paint-color" ).unwrap().unwrap().try_into().unwrap();
     let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
+    let left: i32 = js! (
+        return canvas.offsetLeft;
+    ).try_into().unwrap();
+    let top: i32 = js! (
+        return canvas.offsetTop;
+    ).try_into().unwrap();
 
     canvas.set_width(640 as u32);
     canvas.set_height(480 as u32);
+
+    paint_color.add_event_listener( enclose!((paint_color, context) move |_: ChangeEvent| {
+        let color: String = js! (
+            return @{paint_color.as_ref()}.value;
+        ).try_into().unwrap();
+        let mut hex_color: String = String::from("#");
+        hex_color.push_str(&color);
+        context.set_fill_style_color(&hex_color);
+    }));
 
     window().add_event_listener( enclose!( (canvas) move |_: ResizeEvent| {
          canvas.set_width(canvas.offset_width() as u32);
@@ -109,21 +128,56 @@ fn main() {
     ws.add_event_listener( enclose!( (context, output_msg) move |event: SocketMessageEvent| {
         let data: Vec<u8> = Vec::from(event.data().into_array_buffer().unwrap());
         let decoded_pixel: PaintPixel = deserialize(&data).unwrap();
+        let mut hex_color: String = String::from("#");
+        hex_color.push_str(&format!("{:X}", decoded_pixel.r));
+        hex_color.push_str(&format!("{:X}", decoded_pixel.g));
+        hex_color.push_str(&format!("{:X}", decoded_pixel.b));
+
+        let old_color: String = match context.get_fill_style() {
+            CanvasStyle::String(s) => {
+                s
+            },
+            _ => {
+                String::from("#000000")
+            }
+        };
+
+        context.set_fill_style_color(&hex_color);
         context.fill_rect(f64::from(decoded_pixel.x - 5), f64::from(decoded_pixel.y - 5)
                     , 10.0, 10.0);
+        context.set_fill_style_color(&old_color);
         output_msg(&format!("Received pixel: {:?}", decoded_pixel));
     }));
 
-    canvas.add_event_listener( enclose!( (context, mouse_down) move |event: MouseMoveEvent| {
+    canvas.add_event_listener( enclose!( (context, mouse_down, left) move |event: MouseMoveEvent| {
         if mouse_down.load(Ordering::Relaxed) {
-            context.fill_rect(f64::from(event.client_x() - 5), f64::from(event.client_y() - 5)
+            let x = event.client_x() - left;
+            let y = event.client_y() - top;
+            context.fill_rect(f64::from(x), f64::from(y)
                     , 10.0, 10.0);
+            let color: String = match context.get_fill_style() {
+                CanvasStyle::String(s) => {
+                    s
+                },
+                _ => {
+                    String::from("#000000")
+                }
+            };
+
+            js! ( @(no_return)
+                console.log(@{&color});
+            );
+
+            let r = i64::from_str_radix(&color[1..3], 16).unwrap();
+            let g = i64::from_str_radix(&color[3..5], 16).unwrap();
+            let b = i64::from_str_radix(&color[5..7], 16).unwrap();
+
             let current_pixel = PaintPixel {
-                x: event.client_x(),
-                y: event.client_y(),
-                r: MAX,
-                g: MAX,
-                b: MAX,
+                x: x,
+                y: y,
+                r: r as u8,
+                g: g as u8,
+                b: b as u8,
             };
             let ser: Vec<u8> = serialize(&current_pixel).unwrap();
             let ab: ArrayBuffer = TypedArray::from(ser.as_slice()).buffer();
